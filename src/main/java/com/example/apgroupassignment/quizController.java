f2db81925f04517f06a65f67f52089e001f68a91
kadotaislost <sachin2211068@iimscollege.edu.np>
2023-11-09 14:22:43 +0545

    quiz almost completed

--- src/main/java/com/example/apgroupassignment/quizController.java
+++ src/main/java/com/example/apgroupassignment/quizController.java
@@ -1,30 +1,36 @@
 package com.example.apgroupassignment;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class quizController {
-    private List<String> questions;
+    private List<String[]> questionsWithOptions;
+    private int currentQuestionIndex = 0;
 
     @FXML
     private Label question;
     @FXML
-    private RadioButton opt1,opt2,opt3,opt4;
+    private RadioButton opt1, opt2, opt3, opt4;
+
+    @FXML
+    private ToggleGroup optionsGroup;
 
     @FXML
     private Button prev, next;
 
     @FXML
-    private Label name,gender,date,timer;
-
-    private int currentQuestionIndex = 0;
-
+    private Label name, gender, date, timer;
 
     @FXML
     private void initialize() {
@@ -33,22 +39,15 @@ public class quizController {
     }
 
     private void loadQuestions() {
-        questions = new ArrayList<>();
+        questionsWithOptions = new ArrayList<>();
         try {
             InputStream inputStream = getClass().getResourceAsStream("/mcq.txt");
             if (inputStream != null) {
                 try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-                    StringBuilder questionBuilder = new StringBuilder();
                     String line;
                     while ((line = br.readLine()) != null) {
-                        if (line.endsWith("?")) {
-                            // If the line ends with a question mark, it's the end of a question.
-                            questionBuilder.append(line);  // Append the question
-                            questions.add(questionBuilder.toString());
-                            questionBuilder.setLength(0);  // Clear the builder for the next question
-                        } else {
-                            questionBuilder.append(line).append("\n");  // Append lines of the question
-                        }
+                        String[] questionWithOptions = line.split("\\|");
+                        questionsWithOptions.add(questionWithOptions);
                     }
                 }
             } else {
@@ -58,12 +57,17 @@ public class quizController {
             e.printStackTrace();
         }
     }
-
-
+  
     private void showNextQuestion() {
-        if (currentQuestionIndex < questions.size()) {
-            String currentQuestion = questions.get(currentQuestionIndex);
-            question.setText( questions.get(17));
+        if (currentQuestionIndex < questionsWithOptions.size()) {
+            String[] currentQuestion = questionsWithOptions.get(currentQuestionIndex);
+
+            question.setText(currentQuestion[0]);
+            opt1.setText(currentQuestion[1]);
+            opt2.setText(currentQuestion[2]);
+            opt3.setText(currentQuestion[3]);
+            opt4.setText(currentQuestion[4]);
+
             currentQuestionIndex++;
         } else {
             // Handle the case when all questions have been displayed
@@ -71,5 +75,31 @@ public class quizController {
         }
     }
 
+    public void handlePrevButton(ActionEvent actionEvent) {
+        if (currentQuestionIndex > 0) {
+            currentQuestionIndex--;
+            showCurrentQuestion();
+        } else {
+            // Handle the case when there are no previous questions
+            System.out.println("No previous question available.");
+        }
+    }
+
+    public void handleNextButton(ActionEvent actionEvent) {
+        showNextQuestion();
+    }
+
+
+    private void showCurrentQuestion() {
+        String[] currentQuestion = questionsWithOptions.get(currentQuestionIndex);
+
+        question.setText(currentQuestion[0]);
+        opt1.setText(currentQuestion[1]);
+        opt2.setText(currentQuestion[2]);
+        opt3.setText(currentQuestion[3]);
+        opt4.setText(currentQuestion[4]);
+    }
+
 
 }
+
